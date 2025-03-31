5b3a1e6703e59993db3c610968d15e008c74ec2f
Gabrielly <gabriellycs@al.insper.edu.br>
2025-03-31 01:09:38 -0300

    exe3

--- exe3/main.c
+++ exe3/main.c
@@ -25,13 +25,24 @@ void data_task(void *p) {
 
 void process_task(void *p) {
     int data = 0;
+    int list[5] = {0, 0, 0, 0, 0};
+    int i = 0;
+    int sum = 0;
 
     while (true) {
         if (xQueueReceive(xQueueData, &data, 100)) {
             // implementar filtro aqui!
-
-
-
+            // por enquanto, só imprime os dados recebidos
+            // printf("Data: %d\n", data);
+            if (i < 4) {
+                i++;
+            } else {
+                i = 0;
+            }
+            list[i] = data;
+            sum = list[0] + list[1] + list[2] + list[3] + list[4];
+            sum = sum / 5;
+            printf("Media: %d\n", sum);
 
             // deixar esse delay!
             vTaskDelay(pdMS_TO_TICKS(50));
